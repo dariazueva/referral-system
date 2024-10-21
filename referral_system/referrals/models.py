68961a1be2e134e6310597d7994e0a4b562c64c6
Daria <DZueva26@gmail.com>
2024-10-21 09:34:43 +0500

    change models

--- referral_system/referrals/models.py
+++ referral_system/referrals/models.py
@@ -1,10 +1,20 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 from django.utils import timezone
 
 
+class CustomUser(AbstractUser):
+    email = models.EmailField('Адрес электронной почты', unique=True)
+    groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='customuser_set', blank=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
 class ReferralCode(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='referral_code')
     code = models.CharField(max_length=20, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     expires_at = models.DateTimeField()
@@ -18,8 +28,8 @@ class ReferralCode(models.Model):
 
 
 class Referral(models.Model):
-    referrer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='referrals')
-    referred = models.ForeignKey(User, on_delete=models.CASCADE, related_name='referred_by')
+    referrer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='referrals')
+    referred = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='referred_by')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
